1abedfb3b2dea5a2c3911b7ecb2a5d125cdf870a
young <behappydev@gmail.com>
2015-02-23 14:23:43 -0800

    update 2015-02-23-14:23:41 -

---
+++ AndroidWebViewNotification/gen/com/example/androidwebviewexample/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package com.example.androidwebviewexample;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
---
+++ AndroidWebViewNotification/src/com/example/androidwebview/WebActivity.java
@@ -0,0 +1,27 @@
+package com.example.androidwebview;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.webkit.WebView;
+
+public class WebActivity extends Activity {
+
+	private WebView webView;
+
+	public void onCreate(Bundle savedInstanceState) {
+
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.webcontent);
+
+		webView = (WebView) findViewById(R.id.webView);
+
+		webView.getSettings().setJavaScriptEnabled(true);
+
+		webView.loadUrl("http://m.24meals.com");
+
+		//String customHtml = "<html><body><h2> TEST HTML </h2></body></html>";
+		//webView.loadData(customHtml, "text/html", "UTF-8");
+
+	}
+
+}
